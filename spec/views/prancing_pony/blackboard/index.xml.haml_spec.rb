12db2b4ca1011fa0a7843c08e652b6927ae9892b
Julian Bäume <julian@svg4all.de>
2011-10-21 06:43:27 +0200

    add specs for blackboard index xml
    the views are even implemented, but the model is not complete, yet. so
    the specs are passing, but it won’t work in practice (cucumber will
    still fail)

--- spec/views/prancing_pony/blackboard/index.xml.haml_spec.rb
+++ spec/views/prancing_pony/blackboard/index.xml.haml_spec.rb
@@ -3,8 +3,24 @@ require 'nokogiri'
 
 describe "prancing_pony/blackboard/index.xml.haml" do
   it "should have one blackboard document element" do
+    assign(:users, [])
     render
     doc = Nokogiri::XML.parse(rendered)
     doc.root.name.should == "blackboard"
+  end
+
+  let(:mock_users) do
+    [ double(User, {:nick => 'Joe', :drunk => 24, :debit => -23}),
+      double(User, {:nick => 'Matze', :drunk => 9001, :debit => 100})
+    ]
+  end
+
+  it "should list all users" do
+    assign(:users, mock_users)
+    render
+    doc = Nokogiri::XML.parse(rendered)
+    doc.root.element_children.should have(2).items
+    users = doc.root > ('user')
+    users.should have(2).items
   end
 end
