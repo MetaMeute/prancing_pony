194b1fcdebd1c17ff240f49f62840988bb8264da
Julian Bäume <julian@svg4all.de>
2011-11-09 00:54:10 +0100

    add a table head to the blackboard
    this data will be shown in a table to get an overview

--- spec/views/prancing_pony/blackboard/index.html.haml_spec.rb
+++ spec/views/prancing_pony/blackboard/index.html.haml_spec.rb
@@ -1,5 +1,21 @@
 require 'spec_helper'
 
-describe "blackboard/index.html.haml" do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe "prancing_pony/blackboard/index.html.haml" do
+
+  describe "the blackboard table" do
+    before(:each) { render }
+
+    describe "the table head" do
+      it "should have a name column" do
+        rendered.should have_css("table#blackboard th", :text => "Name")
+      end
+      it "should have a drunk column" do
+        rendered.should have_css("table#blackboard th", :text => "Drunk")
+      end
+      it "should have a debit column" do
+        rendered.should have_css("table#blackboard th", :text => "Debit")
+      end
+    end
+
+  end
 end
