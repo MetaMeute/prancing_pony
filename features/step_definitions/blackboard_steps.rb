e2c148d41a81944e048a6ae75912dc93ac281722
Julian Bäume <julian@svg4all.de>
2011-11-08 23:01:04 +0100

    implement a step definition to set up users

--- features/step_definitions/blackboard_steps.rb
+++ features/step_definitions/blackboard_steps.rb
@@ -1,5 +1,17 @@
+Given /^I do nothing$/ do
+end
+
 Given /^the counter for "([^"]*)" is at "([^"]*)"$/ do |good, amount|
   PrancingPony::Stock.create!(:name => good) unless PrancingPony::Stock.find_by_name(good)
   PrancingPony::Stock.find_by_name(good).amount = amount
+end
+
+Given /^the blackboard lists these values:$/ do |table|
+  table.hashes.each do |row|
+    u = User.create!(:nick => row[:name])
+    u.drunk = row[:drunk]
+    u.debit = row[:debit]
+    u.save!
+  end
 end
 
