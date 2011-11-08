24c52f5a38fc0e3a6cd23be080bdab78d40a8092
Julian Bäume <julian@svg4all.de>
2011-11-08 22:58:54 +0100

    add rspecs for stock model
    this just worked, because of the rails ORM-mapper. but there should be
    some rspec coverage of this code, too. so here it is.

--- spec/models/prancing_pony/stock_spec.rb
+++ spec/models/prancing_pony/stock_spec.rb
@@ -1,5 +1,16 @@
 require 'spec_helper'
 
 describe PrancingPony::Stock do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "can store a good" do
+    PrancingPony::Stock.create!(:name => "Club-Mate")
+    PrancingPony::Stock.find_by_name("Club-Mate").should_not be_nil
+  end
+
+  it "can store amounts for goods" do
+    PrancingPony::Stock.create!(:name => "Club-Mate")
+    g = PrancingPony::Stock.find_by_name("Club-Mate")
+    g.amount = 100
+    g.save!
+    g.amount.should eq(100)
+  end
 end
