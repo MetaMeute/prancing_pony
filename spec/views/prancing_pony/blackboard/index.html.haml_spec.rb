b0f48fc5e600f6d3f728e1f46d592a6b0497a936
Julian Bäume <julian@svg4all.de>
2011-11-09 03:38:38 +0100

    implement a list with items in the stock

--- spec/views/prancing_pony/blackboard/index.html.haml_spec.rb
+++ spec/views/prancing_pony/blackboard/index.html.haml_spec.rb
@@ -3,12 +3,12 @@ require 'spec_helper'
 describe "prancing_pony/blackboard/index.html.haml" do
 
   describe "the blackboard table" do
-    before(:each) do
-      assign :users, User.all
-      render
-    end
-
     describe "the table head" do
+      before(:each) do
+        assign :users, User.all
+        assign :stock, []
+        render
+      end
 
       it "should have a name column" do
         rendered.should have_css("table#blackboard th", :text => "Name")
@@ -27,6 +27,7 @@ describe "prancing_pony/blackboard/index.html.haml" do
         User.create!(:nick => "Mike", :drunk => 42, :debit => 23)
         User.create!(:nick => "Matze", :drunk => 9001, :debit => 1337)
         assign :users, User.all
+        assign :stock, []
         render
       end
 
@@ -37,6 +38,21 @@ describe "prancing_pony/blackboard/index.html.haml" do
           rendered.should have_css("table#blackboard td##{u.nick}_debit", :text => u.debit.to_s)
         end
       end
+    end
+  end
+
+  describe "the stock overview" do
+    before(:each) do
+      PrancingPony::Stock.create!(:name => "Club-Mate", :amount => 23)
+      PrancingPony::Stock.create!(:name => "Beer", :amount => 42)
+      assign :users, []
+      assign :stock, PrancingPony::Stock.all
+      render
+    end
+
+    it "should render information about the stock" do
+      rendered.should have_content("42.0 bottles of Beer left")
+      rendered.should have_content("23.0 bottles of Club-Mate left")
     end
   end
 end
