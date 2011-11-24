f916c5345c8212e64aba39764a607e0b535e0654
Julian Bäume <julian@svg4all.de>
2011-11-25 00:52:18 +0100

    first implementation of drink and pay actions
    pay doesn’t do anything for now

--- app/controllers/prancing_pony/blackboard_controller.rb
+++ app/controllers/prancing_pony/blackboard_controller.rb
@@ -1,9 +1,33 @@
 module PrancingPony
   class BlackboardController < ApplicationController
+
+    before_filter :find_user, :only => [:drink, :pay]
+
     def index
       @users = User.all
       @stock = Stock.where('amount != 0').all
     end
   
+    def drink
+      return if @user.nil?
+
+      @user.drink!
+
+      redirect_to blackboard_path
+    end
+
+    def pay
+      return if @user.nil?
+    end
+
+    private
+    def find_user
+      @user = User.find_by_nick params[:nick]
+
+      if @user.nil? then
+        flash[:error] = "Must provide a nick!"
+        redirect_to root_url
+      end
+    end
   end
 end
