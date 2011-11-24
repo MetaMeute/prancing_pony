075c382508702e707404135c0a45f154d4968256
Julian Bäume <julian@svg4all.de>
2011-11-25 00:51:58 +0100

    add routes to drink and pay actions

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 PrancingPony::Engine.routes.draw do
   get 'blackboard', :controller => :blackboard, :action => :index
 
+  post 'drink', :controller => :blackboard, :action => :drink
+  post 'pay', :controller => :blackboard, :action => :pay
+
   root :to => 'blackboard#index'
 end
